2fa9d3be438a9bb96a60a2fcd8c7eb7637ccfbb9
kVest27 <kvest27@yandex.ru>
2024-11-30 19:46:53 +0700

    Update client.py

--- flaskapp/client.py
+++ flaskapp/client.py
@@ -1,4 +1,6 @@
 import requests
+import os
+import base64
 
 # Тестируем главный маршрут
 r = requests.get('http://localhost:5000/')
@@ -9,3 +11,17 @@ print(r.text)
 r = requests.get('http://localhost:5000/data_to')
 print(r.status_code)
 print(r.text)
+
+path = os.path.join('./static', 'image0008.png')
+
+with open(path, 'rb') as fh:
+    img_data = fh.read()
+    b64 = base64.b64encode(img_data)
+
+jsondata = {'imagebin': b64.decode('utf-8')}
+res = requests.post('http://localhost:5000/apinet', json=jsondata)
+
+if res.ok:
+    print(res.json())
+else:
+    print(f"Ошибка: {res.status_code}, {res.text}")
