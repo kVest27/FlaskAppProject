a1fd87fc359439d69807f2346868e93cc3a39148
kVest27 <kvest27@yandex.ru>
2024-11-30 19:31:29 +0700

    Update client.py

--- flaskapp/client.py
+++ flaskapp/client.py
@@ -1,5 +1,11 @@
 import requests
 
-r = requests.get('http://127.0.0.1:5000/')
+# Тестируем главный маршрут
+r = requests.get('http://localhost:5000/')
+print(r.status_code)
+print(r.text)
+
+# Тестируем маршрут /data_to
+r = requests.get('http://localhost:5000/data_to')
 print(r.status_code)
 print(r.text)
