95657bd988753a9ef95513a124274ed3435187f4
kVest27 <kvest27@yandex.ru>
2024-11-30 19:25:51 +0700

    Update st.sh

--- flaskapp/st.sh
+++ flaskapp/st.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 gunicorn --bind 127.0.0.1:5000 wsgi:app & APP_PID=$!
 sleep 5
+echo "Start client"
+python3 client.py
+sleep 5
 echo $APP_PID
 kill -TERM $APP_PID
 echo "Gunicorn process terminated"
